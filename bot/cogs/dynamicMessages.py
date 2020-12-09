a090c6f2a3dcd6ae88e235a4150b5702eb135258
mircearoata <mircearoatapalade@yahoo.ro>
2020-12-09 21:59:42 +0200

    Untrack deleted dynamic messages

--- bot/cogs/dynamicMessages.py
+++ bot/cogs/dynamicMessages.py
@@ -18,25 +18,46 @@ class DynamicMessages(commands.Cog):
     day = datetime.now().strftime('%A')
     embed = embeds.make_courses_embed(day)
     dynamicMessages = config.get('dynamicGetCourses', [])
+    toRemove = []
     for messageData in dynamicMessages:
-      message = await self.bot.get_channel(int(messageData[0])).fetch_message(int(messageData[1]))
-      if message:
+      try:
+        message = await self.bot.get_channel(int(messageData[0])).fetch_message(int(messageData[1]))
         await message.edit(embed=embed)
+      except:
+        toRemove.append(messageData)
+    for remove in toRemove:
+      dynamicMessages.remove(remove)
+    if len(toRemove) > 0:
+      config.save('dynamicGetCourses', dynamicMessages)
 
   @tasks.loop(seconds=5.0)
   async def update_dynamic_current_course(self):
     embed = embeds.make_current_course_embed()
     dynamicMessages = config.get('dynamicCurrentCourse', [])
+    toRemove = []
     for messageData in dynamicMessages:
-      message = await self.bot.get_channel(int(messageData[0])).fetch_message(int(messageData[1]))
-      if message:
+      try:
+        message = await self.bot.get_channel(int(messageData[0])).fetch_message(int(messageData[1]))
         await message.edit(embed=embed)
+      except:
+        toRemove.append(messageData)
+    for remove in toRemove:
+      dynamicMessages.remove(remove)
+    if len(toRemove) > 0:
+      config.save('dynamicCurrentCourse', dynamicMessages)
 
   @tasks.loop(seconds=5.0)
   async def update_dynamic_next_course(self):
     embed = embeds.make_next_course_embed()
     dynamicMessages = config.get('dynamicNextCourse', [])
+    toRemove = []
     for messageData in dynamicMessages:
-      message = await self.bot.get_channel(int(messageData[0])).fetch_message(int(messageData[1]))
-      if message:
+      try:
+        message = await self.bot.get_channel(int(messageData[0])).fetch_message(int(messageData[1]))
         await message.edit(embed=embed)
+      except:
+        toRemove.append(messageData)
+    for remove in toRemove:
+      dynamicMessages.remove(remove)
+    if len(toRemove) > 0:
+      config.save('dynamicNextCourse', dynamicMessages)
